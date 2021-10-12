40de106c3dd656634e2f9d4d03334a212d61ab9e
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-12 13:33:46 +0545

    Update for new routes

--- Core/Request.php
+++ Core/Request.php
@@ -10,4 +10,16 @@ class Request {
     public function getUri() {
         return rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
     }
+
+    public function getInput() {
+        $data = [];
+            
+        foreach ($_POST as $key => $value) {
+            $data[$key] = filter_input(
+                INPUT_POST, 
+                $key, 
+                FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+        return $data;
+    }
 }
