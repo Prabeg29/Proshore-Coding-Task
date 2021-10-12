3e1ed3bfcd386a7ce82fc5befaf82fb2928c459d
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-12 09:34:39 +0545

    Instantiate the application

---
+++ Core/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Core;
+
+class Application{
+    public function run() {
+        echo "Hello World";
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Core\Application;
+
+require 'vendor/autoload.php';
+
+$app = new Application();
+$app->run();
