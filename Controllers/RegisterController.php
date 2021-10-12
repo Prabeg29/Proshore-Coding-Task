fe88bff26ae089776b2d3f3edc4aef5f730bcfab
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-12 13:35:24 +0545

    Implement user registration logic

---
+++ Controllers/RegisterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Request;
+use App\Models\User;
+
+class RegisterController {
+    protected User $user;
+
+    public function __construct()
+    {
+        $this->user = new User();
+    }
+
+    public function index() {
+        require 'Views/register.view.php';
+    }
+
+    public function store(Request $request) {
+        $userData = $request->getInput();
+
+        $this->user->createUser([
+            'username' => $userData['username'],
+            'email' => $userData['email'], 
+            'password' => $userData['password']
+        ]);
+    }
+}
