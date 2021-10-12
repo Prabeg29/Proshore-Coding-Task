1c7c8de30ac5c5426851b5c6c39263c26662b543
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-12 13:34:52 +0545

    Create user model

--- Core/Database.php
+++ Core/Database.php
@@ -16,9 +16,31 @@ class Database {
 
         try{
             $this->pdo = new PDO($dsn, $username, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(PDOException $ex){
             die($ex->getMessage());
+        }
+    }
+
+    protected function prepare($sql) {
+        return $this->pdo->prepare($sql);
+    }
+
+    public function run($sql, $params = null) {
+        $stmt = $this->prepare($sql);
+
+        if(!is_null($params)){
+            foreach ($params as $key => $value) {
+                $stmt->bindValue(":$key", $value);
+            }
+        }
+        
+        try{
+            $stmt->execute();
+        }
+        catch(PDOException $ex){
+            echo $ex->getMessage();
         }
     }
 }
