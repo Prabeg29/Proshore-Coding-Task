e53dc7b63fe2798daf0158be8041155b9c9980ec
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-13 16:04:59 +0545

    Update views for delete post

--- Views/post.view.php
+++ Views/post.view.php
@@ -10,5 +10,15 @@
     <p>
         <?= htmlspecialchars($data->body); ?>
     </p>
+    <?php if(App\Core\Session::get('user_id')):?>
+        <form action="/posts/<?= $data->id?>" method="POST">
+            <input type="hidden" name="_method" value="PUT">
+            <button type="submit">Edit</button>
+        </form>
+        <form action="/posts/<?= $data->id?>" method="POST">
+            <input type="hidden" name="_method" value="DELETE">
+            <button type="submit">Delete</button>
+        </form>
+    <?php endif?>
 </div>
 <?php require 'Views/templates/foot.php'?>
