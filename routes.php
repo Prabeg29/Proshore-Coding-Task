b8b872bf150357e9105a50a8688411b58bf81781
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-13 10:10:46 +0545

    Add route to create post

--- routes.php
+++ routes.php
@@ -13,3 +13,6 @@ Router::get('/users/register', [RegisterController::class, 'index']);
 Router::post('/users/register', [RegisterController::class, 'store']);
 Router::get('/users/login', [LoginController::class, 'index']);
 Router::post('/users/login', [LoginController::class, 'login']);
+
+Router::get('/posts', [PostController::class, 'index']);
+Router::post('/posts', [PostController::class, 'store']);
