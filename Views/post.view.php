9445ca0eacd2c8bf9c2835b21c96d23ec96a0363
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-16 17:42:23 +0545

    Update view for single post

--- Views/post.view.php
+++ Views/post.view.php
@@ -5,16 +5,16 @@
         <?= htmlspecialchars($data->title); ?>
     </h2>
     <div class="date">
-        <?= date($data->created_at)?>
+        <?= $data->username?>
+    </div>
+    <div class="date">
+        <?= $data->updated_at?>
     </div>
     <p>
-        <?= htmlspecialchars($data->body); ?>
+        <?= htmlspecialchars($data->description); ?>
     </p>
-    <?php if(App\Core\Session::get('user_id')):?>
-        <form action="/posts/<?= $data->id?>" method="POST">
-            <input type="hidden" name="_method" value="PUT">
-            <button type="submit">Edit</button>
-        </form>
+    <?php if(App\Core\Session::get('user_id') && App\Core\Session::get('user_id')=== $data->user_id):?>
+        <a href="/posts-edit/<?= $data->id?>">Edit</a>
         <form action="/posts/<?= $data->id?>" method="POST">
             <input type="hidden" name="_method" value="DELETE">
             <button type="submit">Delete</button>
