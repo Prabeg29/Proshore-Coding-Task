a66df7372a00538479e36d7a385df1b5af869003
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-16 17:44:13 +0545

    Redefine urls for top menu

--- Views/templates/top-menu.php
+++ Views/templates/top-menu.php
@@ -1,16 +1,22 @@
+<?php
+define('PORT', '8080');
+?>
+
 <div class="top-menu">
     <div class="menu-options">
         <?php
             if(App\Core\Session::get('user_id')):
         ?>
-            <a href="/">Home</a>
+            <a href="http://localhost:<?= PORT?>/">Home</a>
             |
-            <a href="/posts">New Post</a>
+            <a href="http://localhost:<?= PORT?>/posts">New Post</a>
             |
             <?= "Welcome ". htmlspecialchars(\App\Core\Session::get('username')); ?>
-            <a href="users/logout">Logout</a>
+            <a href="http://localhost:<?= PORT?>/users/logout">Logout</a>
         <?php else:?>
-            <a href="users/login">Login</a>
+            <a href="http://localhost:<?= PORT?>/users/login">Login</a>
+            |
+            <a href="http://localhost:<?= PORT?>/users/register">Register</a>
         <?php endif; ?>
     </div>
 </div>
