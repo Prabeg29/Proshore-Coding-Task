308dae586bbb8ba37974ed6c5092fca40a4d5896
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-10-17 13:28:41 +0545

    Implement DB logic for pagination to fetch all active posts

--- Models/Post.php
+++ Models/Post.php
@@ -7,9 +7,49 @@ use PDO;
 
 class Post extends Model{
     protected const TABLE = 'posts';
+    protected int $totalRecords;
+    protected int $limit = 3;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTotalRecords();
+    }
+
+    public function setTotalRecords() {
+        $sql   = "SELECT id FROM ".self::TABLE." WHERE status=1";
+        $stmt = $this->db->run($sql); 
+        $this->totalRecords = $stmt->rowCount();
+    }
+
+    public function getCurrentPage() {
+        return isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    }
+
+    public function getPaginationNumber() {
+        return ceil($this->totalRecords / $this->limit);
+    }
 
     public function getAllPosts() {
-        $sql = "SELECT posts.id, posts.title, posts.description, posts.status, posts.updated_at, posts.user_id, users.username FROM ".self::TABLE." INNER JOIN users ON ".self::TABLE.".user_id=users.id WHERE status=1 ORDER BY posts.updated_at DESC";
+        $offSet = 0;
+        if($this->getCurrentPage() > 1){
+            $offSet = ($this->getCurrentPage() * $this->limit) - $this->limit;
+        }
+
+        $sql = "SELECT 
+            posts.id, 
+            posts.title, 
+            posts.description, 
+            posts.status, 
+            posts.updated_at, 
+            posts.user_id, 
+            users.username 
+            FROM ".self::TABLE.
+            " INNER JOIN users
+             ON ".self::TABLE.".user_id=users.id 
+             WHERE status=1 
+             ORDER BY posts.updated_at DESC 
+             LIMIT $offSet, $this->limit";
 
         $stmt = $this->db->run($sql);
 
